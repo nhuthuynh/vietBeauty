7c9ab71a1c94374a69f77f934277d31683def33d
nhuthuynh <ne7ven@yahoo.com>
2015-03-23 03:04:30 +0700

    update responsive

--- js/script.js
+++ js/script.js
@@ -6,13 +6,23 @@ $(document).ready(function() {
         pager: '.pbs-controls',
         activePagerClass: 'pbs-activeSlide',
         before: function() {
-            
+
         },
         after: function() {
-        	$('.pbs-details', arguments[0]).removeClass('pbs-show');
-        	$('.pbs-pattern', arguments[0]).removeClass('pbs-dd');
-        	$('.pbs-details', arguments[1]).addClass('pbs-show');
-        	$('.pbs-pattern', arguments[1]).addClass('pbs-dd');
+            $('.pbs-details', arguments[0]).removeClass('pbs-show');
+            $('.pbs-pattern', arguments[0]).removeClass('pbs-dd');
+            $('.pbs-details', arguments[1]).addClass('pbs-show');
+            $('.pbs-pattern', arguments[1]).addClass('pbs-dd');
         }
+    });
+
+    $('.btn-mobile-menu').bind('click', function(e) {
+        e.preventDefault();
+        $('.mobile-nav').toggleClass('active');
+    });
+
+    $('.nav-click').bind('click', function(e) {
+        e.preventDefault();
+        $(this).siblings('.mobile-nav-submenu').toggleClass('active');
     });
 });
